afcf56dafb5352d2746e9511e54b094e03b28c5a
fariha7758 <hossain7758@gmail.com>
2021-05-11 14:38:49 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ driver.get('https://www.wg-gesucht.de/wg-zimmer-in-Passau.104.0.1.0.html')
 links_dict ={}
 results_dict ={}
 
-if(driver.find_element_by_class_name('card_body')):
+if(driver.find_elements_by_class_name('card_body')):
     print('Class found')
 else:
     print('No class found')
@@ -17,7 +17,7 @@ results = driver.find_elements_by_class_name('card_body')
 links = driver.find_elements_by_class_name('truncate_title')
 #print('links', links)
 for num in range(len(links)):
-
+    #print("link_list", links[num].find_element_by_tag_name('a').get_attribute('innerText'))
     links_dict[links[num].find_element_by_tag_name('a').get_attribute('href')]=num
 print(len(links_dict))
 for count in range(len(results)):
@@ -32,14 +32,15 @@ for num in range(len(titles_list)):
 print(list1)
 my_pages = pd.DataFrame(list1, columns=['Description', 'Links'])
 my_pages.to_csv('dataset.csv')
-#content_dict =[]
-#for link in links_dict:
-    #driver.get(link)
+content_dict =[]
+for link in links_dict:
+    driver.get(link)
 
-    #content= driver.find_elements_by_id('freitext_2_content')
-    #print(content.find_element_by_tag_name('p').get_attribute('innerHTML'))
-   # content_dict.append(content)
-#print(content_dict)
+    content= driver.find_elements_by_id('freitext_2_content')
+    for item in content:
+        print("link_list", item.get_attribute('innerHTML'))
+        content_dict.append(item.get_attribute('innerHTML'))
+print(len(content_dict),len(links_list), len(titles_list))
 #print(results_dict)
 #print(links_dict)
 #results_dict.update(links_dict)
